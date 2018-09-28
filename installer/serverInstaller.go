73178d3f0485d4b6a90d11ee56796795ede53912
mathias sebuuma <mathias240@live.com>
2018-09-28 11:47:18 +0300

    changes

--- installer/serverInstaller.go
+++ installer/serverInstaller.go
@@ -2,11 +2,8 @@ package installer
 
 import (
 	"airdb/helpers"
-	"bytes"
 	"fmt"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 func serverInstaller() {
@@ -47,44 +44,10 @@ func serverInstaller() {
 	if err != nil {
 		fmt.Println("Failed to create and write supervisorctl conf file error: " + err.Error())
 		os.Exit(103)
-	}
-	fmt.Println("Reloading supervisor with new airdb conf settings...")
-	cmd1 := exec.Command("/bin/bash", "-c", "supervisorctl restart")
-	var outb, errb bytes.Buffer
-	cmd1.Stdout = &outb
-	cmd1.Stderr = &errb
-	err = cmd1.Run()
-	if err != nil {
-		if err.Error() == "exit status 127" {
-			fmt.Println("Error: Install supervisor.d to continue with this installation")
-			os.Exit(103)
-		} else {
-			fmt.Println("Exception occured while reloading supervisor conf: " + err.Error())
-			os.Exit(103)
-		}
-	}
-	fmt.Println("Starting airdb web service...")
-	cmd2 := exec.Command("/bin/bash", "-c", "supervisorctl restart airdb")
-	cmd2.Stdout = &outb
-	cmd2.Stderr = &errb
-	err = cmd2.Run()
-	if err != nil {
-		if err.Error() == "exit status 127" {
-			fmt.Println("Error: Install supervisor.d to continue with this installation")
-			os.Exit(103)
-		} else {
-			fmt.Println("Exception occured while reloading supervisor conf: " + err.Error())
-			os.Exit(103)
-		}
-	}
-	if strings.Contains(outb.String(), "(no such process)") {
-		fmt.Println("Failed to start airdb application. Check your supervisorctl settings")
-		os.Exit(103)
 	} else {
 		fmt.Println("Airdb configured successfully")
 		os.Exit(103)
 	}
-
 }
 
 func createConfFile(path string) error {
